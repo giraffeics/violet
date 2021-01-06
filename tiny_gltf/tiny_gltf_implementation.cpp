5efe61a8928d7b53e1213132c61c0c32891f744a
giraffeics <purplegira@gmail.com>
2021-01-05 22:33:13 -0500

    add tiny_gltf to the repository

---
+++ tiny_gltf/tiny_gltf_implementation.cpp
@@ -0,0 +1,7 @@
+// This code file will invoke the implementations
+// of the various components of the GLTF library.
+
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "tiny_gltf.h"
