1b4cae76c2781f76d0daaf5132d56c0f56425042
Frank <purplegira@gmail.com>
2021-02-02 23:09:40 -0500

    Add documentation for the GPUProcess class

--- src/GPUProcess.cpp
+++ src/GPUProcess.cpp
@@ -4,59 +4,123 @@
 
 // GPUProcess default function implementations
 
+/**
+ * @brief Tell this GPUProcess what GPUEngine to use.
+ * 
+ * @param engine 
+ */
 void GPUProcess::setEngine(GPUEngine* engine)
 {
 	if (mEngine == nullptr)
 		mEngine = engine;
 }
 
+/**
+ * @brief Return an array of the instance extensions this GPUProcess requires.
+ * 
+ * @param count Pointer to which the number of instance extensions should be passed.
+ * @return const char** 
+ */
 const char** GPUProcess::getRequiredInstanceExtensions(uint32_t* count)
 {
 	*count = 0;
 	return nullptr;
 }
 
+/**
+ * @brief Return an array of the device extensions this GPUProcess requires.
+ * 
+ * @param count Pointer to which the number of device extensions should be passed.
+ * @return const char** 
+ */
 const char** GPUProcess::getRequiredDeviceExtensions(uint32_t* count)
 {
 	*count = 0;
 	return nullptr;
 }
 
+/**
+ * @brief Return the type of VkQueue that this GPUProcess needs, if this GPUProcess performs a command buffer operation.
+ * 
+ * @return VkQueueFlags 
+ */
 VkQueueFlags GPUProcess::getNeededQueueType()
 {
 	return 0;
 }
 
+/**
+ * @brief Return the type of operation that this GPUProcess performs.
+ * 
+ * @return GPUProcess::OperationType 
+ */
 GPUProcess::OperationType GPUProcess::getOperationType()
 {
 	return OP_TYPE_COMMAND;
 }
 
+/**
+ * @brief Perform this GPUProcess's operation by allocating a command buffer, recording commands into it, and returning it.
+ * 
+ * If getOperationType() does not return OP_TYPE_COMMAND, this function will never be called.
+ * After this function is called, the GPUDependencyGraph submits the returned command buffer,
+ * and then frees it once it is no longer needed.
+ * 
+ * @param commandPool The command pool from which to allocate a command buffer.
+ * @return VkCommandBuffer 
+ */
 VkCommandBuffer GPUProcess::performOperation(VkCommandPool commandPool)
 {
 	return VK_NULL_HANDLE;
 }
 
+/**
+ * @brief Perform ths GPUProcess's operation, using the given syncronization objects.
+ * 
+ * If getOperationType() does not return OP_TYPE_OTHER, this function will never be called.
+ * 
+ * @param waitSemaphores Zero or more semaphores which the operation must wait on.
+ * @param fence A fence which the operation must signal.
+ * @param semaphore A semaphore which the operation must wait on.
+ * @return true Operation was successful.
+ * @return false Operation was unsuccessful.
+ */
 bool GPUProcess::performOperation(std::vector<VkSemaphore> waitSemaphores, VkFence fence, VkSemaphore semaphore)
 {
 	return true; // do nothing in default implementation
 }
 
+/**
+ * @brief Return a vector of PRDependency instances with entries for every PassableResource this GPUProcess uses.
+ * 
+ * The returned vector must not include PRDependencies for any PassableResources which this GPUProcess owns.
+ * 
+ * @return std::vector<GPUProcess::PRDependency> 
+ */
 std::vector<GPUProcess::PRDependency> GPUProcess::getPRDependencies()
 {
 	return std::vector<PRDependency>();
 }
 
+/**
+ * @brief Acquire all owned resources which will persist for the duration of this GPUProcess's lifetime.
+ */
 void GPUProcess::acquireLongtermResources()
 {
 	// do nothing for default implementation
 }
 
+/**
+ * @brief Acquire all owned resources which may be tied to the GPUEngine's surface.
+ */
 void GPUProcess::acquireFrameResources()
 {
 	// do nothing for default implementation
 }
 
+/**
+ * @brief Free all owned resources which may be tied to the GPUEngine's surface.
+ */
 void GPUProcess::cleanupFrameResources()
 {
 	// do nothing for default implementation
--- src/GPUProcess.h
+++ src/GPUProcess.h
@@ -7,6 +7,34 @@
 
 class GPUEngine;
 
+/**
+ * @brief Base class for all processes which utilize the GPU and can be managed by a GPUDependencyGraph.
+ * 
+ * A GPUProcess can perform an operation by generating a command buffer, doing something
+ * else and signaling the syncronization object(s) passed to it, or can have no operation.
+ * To facilitate this, there are two function signatures for performOperation(), and a
+ * child class can implement one or neither of them; also, a subclass can implement the
+ * getOperationType() function to specify an operation type other than generating a
+ * command buffer.
+ * 
+ * GPUProcesses can pass resources between each other during the execution of the dependency
+ * graph, using the PassableResource and PRDependency nested classes. A GPUProcess which owns such
+ * a resource should also own a PassableResource for that resource. Said PassableResource
+ * is to be updated with possible values upon resource acquisition or re-acquisition,
+ * and updated with the current value when performOperation() is called. If this GPUProcess
+ * has no operation, the current value is to be set upon resource acquisition or re-acquisition.
+ * Owned PassableResource instances should generally be made available to other classes as
+ * a const pointer, through a public getter function.
+ * 
+ * If a GPUProcess uses any PassableResources which are owned by another GPUProcess, it
+ * must override getPRDependencies() so that it can describe what PassableResources it uses,
+ * and at which pipeline stage(s). The GPUDependencyGraph will analyze these relationships to
+ * determine the proper execution order and potentially group command buffer submits together.
+ * 
+ * GPUProcess is not meant to be instantiated; however, there is no single function which
+ * all child classes must override, so I have not found a good way to make this class
+ * pure virtual without adding a lot of clutter to all child class implementation files.
+ */
 class GPUProcess
 {
 public:
@@ -17,7 +45,12 @@ public:
 		OP_TYPE_NOOP
 	};
 
-	// classes & structs scoped to GPUProcess
+	/**
+	 * @brief Base class for PassableResource.
+	 * 
+	 * Provides minimal functionality so that PRDependency instances can be made and used
+	 * without PRDependency needing to be a generic type.
+	 */
 	class PassableResourceBase
 	{
 	public:
@@ -29,13 +62,28 @@ public:
 		// do not allow instances of base class
 		~PassableResourceBase() {};
 
-	protected:
 		GPUProcess* mProcess = nullptr;
 	};
 	
+	/**
+	 * @brief Generic PassableResource class allowing various resource types to be passed safely.
+	 * 
+	 * Intended for the passing of Vulkan resources which have a handle and do not have any associated
+	 * metadata which must also be passed. If there is associated metadata that must be passed along
+	 * with the Vulkan handle, a subclass such as PassableImageView should be used. If there is
+	 * no suitable subclass, a new one may need to be created.
+	 * 
+	 * @tparam T Type of resource handle that will be passed.
+	 */
 	template<typename T> class PassableResource : public PassableResourceBase
 	{
 	public:
+		/**
+		 * @brief Construct a new PassableResource object.
+		 * 
+		 * @param process The GPUProcess responsible for managing the passed handle.
+		 * @param handle Pointer to a variable where the passed handle resides.
+		 */
 		PassableResource(GPUProcess* process, T* handle){
 			mProcess = process;
 			mVkHandle = handle;
@@ -44,6 +92,7 @@ public:
 		T getVkHandle() const {
 			return *mVkHandle;
 		}
+
 		const std::vector<T> getPossibleValues() const {
 			return mPossibleValues;
 		}
@@ -57,9 +106,23 @@ public:
 		std::vector<T> mPossibleValues = {};
 	};
 
+	/**
+	 * @brief A PassableResource subclass for passing VkImageViews and associated metadata.
+	 * 
+	 * Currently, the extent and format of the image view can be passed through this class.
+	 * The GPUProcess which owns a PassableImageView instance is expected to set the format
+	 * and extent whenever they change. The format and extent should only change when resources
+	 * are acquired or re-acquired.
+	 */
 	class PassableImageView : public PassableResource<VkImageView>
 	{
 	public:
+		/**
+		 * @brief Construct a new PassableImageView object.
+		 * 
+		 * @param process The GPUProcess responsible for managing the passed handle and setting associated metadata.
+		 * @param handle Pointer to a variable where the passed handle resides.
+		 */
 		PassableImageView(GPUProcess* process, VkImageView* handle) 
 			: PassableResource<VkImageView>(process, handle) {}
 
@@ -84,6 +147,12 @@ public:
 		VkFormat mFormat;
 	};
 
+	/**
+	 * @brief Structure describing a dependency on a passable resource.
+	 * 
+	 * Contains a const pointer to the passable resource, as well as a set of flags describing
+	 * which pipeline stage(s) the resource is used in.
+	 */
 	struct PRDependency
 	{
 		const PassableResourceBase* resource;
@@ -108,6 +177,11 @@ protected:
 	GPUEngine* mEngine = nullptr;
 };
 
+/**
+ * @brief A virtual subtype of GPUProcess which is used to interface with a windowing API.
+ * 
+ * This functionality may later be made to not use a GPUProcess, or may be merged with GPUProcessSwapchain.
+ */
 class GPUWindowSystem : public GPUProcess
 {
 public:
